fee76517bba0a1de19cfb11e64377852234d251e
Riley Gregg <rtgregg93@gmail.com>
2022-07-27 17:48:49 -0400

    initial layout and operator functions completed

---
+++ script.js
@@ -0,0 +1,39 @@
+function add(x, y) {
+    let answer = x + y;
+    return answer;
+}
+
+function subtract(x, y) {
+    let answer = x - y;
+    return answer;
+}
+
+function multiply(x, y) {
+    let answer = x * y;
+    return answer;
+}
+
+function divide(x, y) {
+    let answer = x / y;
+    return answer;
+}
+
+function operate(operator, x, y) {
+    let answer = "";
+
+    if (operator == "+") {
+        answer = add(x, y);
+    } else if (operator == "-") {
+        answer = subtract(x, y);
+    } else if (operator == `*`) {
+        answer = multiply(x, y);
+    } else if (operator == `/`) {
+        answer = divide(x, y);
+    } else {
+        console.log("choose an operator");
+    }
+
+    return answer;
+}
+
+console.log(operate("+", 10, 6));
