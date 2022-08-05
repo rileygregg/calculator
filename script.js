2a4d39e4a91df623554d0a4a3da2198bbd1839d3
Riley Gregg <rtgregg93@gmail.com>
2022-08-05 13:21:14 -0400

    calculator functioning with only one equation operated on

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Dom elements
 const buttons = document.querySelectorAll("#btn");
+const buttonOperators = document.querySelectorAll("#btn-op");
 const equation = document.querySelector(".container-display");
 const displayResult = document.querySelector(".container-result");
 const clearAll = document.querySelector(".clear-all");
@@ -7,12 +8,7 @@ const clearAll = document.querySelector(".clear-all");
 let x = "";
 let y = "";
 let operator = "";
-
-let stillTyping = false;
-
-// Object.keys(inputs).forEach(function (keys) {
-//     inputs[keys] = parseInt(inputs[keys]);
-// });
+let inputs = [];
 
 // create functions to calculate arguments
 function add(x, y) {
@@ -35,6 +31,18 @@ function divide(x, y) {
     return answer;
 }
 
+function squared(x, y) {
+    let answer = x ** y;
+    return answer;
+}
+
+function remainder(x, y) {
+    let answer = x % y;
+    return answer;
+}
+
+console.log(squared(5, 2));
+
 function operate(operator, x, y) {
     let answer = "";
 
@@ -46,6 +54,10 @@ function operate(operator, x, y) {
         answer = multiply(x, y);
     } else if (operator == `/`) {
         answer = divide(x, y);
+    } else if (operator == `**`) {
+        answer = squared(x, y);
+    } else if (operator == `%`) {
+        answer = remainder(x, y);
     } else {
         console.log("choose an operator");
     }
@@ -53,12 +65,11 @@ function operate(operator, x, y) {
     return (displayResult.textContent = answer);
 }
 
-function makeX() {
-    for (const button of buttons) {
-        button.addEventListener("click", () => {
-            // for x
-            if (
-                button.value == "1" ||
+for (const button of buttons) {
+    button.addEventListener("click", () => {
+        if (
+            // if you select a number and there hasnt been an operator selected yet, this will run
+            (button.value == "1" ||
                 button.value == "2" ||
                 button.value == "3" ||
                 button.value == "4" ||
@@ -67,18 +78,101 @@ function makeX() {
                 button.value == "7" ||
                 button.value == "8" ||
                 button.value == "9" ||
-                button.value == "0"
-            ) {
-                x += button.value;
-                // equation.textContent = x;
-                console.log(x);
+                button.value == "0") &&
+            operator == ""
+        ) {
+            x += button.value;
+            let newX = parseInt(x);
+            x = newX;
 
-                // console.log(x);
-                //
+            inputs[0] = x;
+
+            equation.textContent = x + operator + y;
+            console.log(inputs);
+            return;
+        } else if (
+            (button.value == "+" ||
+                button.value == "-" ||
+                button.value == "*" ||
+                button.value == "/" ||
+                button.value == "**" ||
+                button.value == "%") &&
+            operator == ""
+        ) {
+            operator += button.value;
+            inputs[1] = operator;
+            equation.textContent = x + operator + y;
+            console.log(operator);
+            return;
+        } else if (
+            // if you select a number and there has been an operator selected, this will run
+            (button.value == "1" ||
+                button.value == "2" ||
+                button.value == "3" ||
+                button.value == "4" ||
+                button.value == "5" ||
+                button.value == "6" ||
+                button.value == "7" ||
+                button.value == "8" ||
+                button.value == "9" ||
+                button.value == "0") &&
+            operator !== ""
+        ) {
+            y += button.value;
+            let newY = parseInt(y);
+            y = newY;
+
+            inputs[2] = y;
+            equation.textContent = x + operator + y;
+            console.log(inputs);
+            return;
+        } else if (button.value == "CA") {
+            x = "";
+            y = "";
+            operator = "";
+
+            inputs = [];
+
+            displayResult.textContent = "";
+            equation.textContent = "";
+            console.log(inputs);
+        } else if (button.value == "DEL") {
+            console.log(x);
+            // turns into array
+            x = x.split("");
+            console.log(x);
+            // removes last item in array
+            x.pop();
+            console.log(x);
+            // create new variable with new array without last item
+            newX = x.join("");
+            // convert new variable to old variable as string
+            x = newX;
+            console.log(x);
+            equation.textContent = x;
+        } else {
+            let result = operate(operator, x, y);
+            displayResult.textContent = result;
+            inputs[3] = result;
+        }
+        console.log(inputs);
+        equation.textContent = x + operator + y;
+    });
+}
+
+/*} else if (
+                button.value == "+" ||
+                button.value == "-" ||
+                button.value == "*" ||
+                button.value == "/" ||
+                button.value == "**" ||
+                button.value == "%"
+            ) {
+                createOperator();
             } else if (button.value == "CA") {
                 x = "";
-                // displayResult.textContent = x;
-                // equation.textContent = x;
+                displayResult.textContent = x;
+                equation.textContent = x;
             } else if (button.value == "DEL") {
                 console.log(x);
                 // turns into array
@@ -97,79 +191,8 @@ function makeX() {
                 // displayResult.textContent = x;
                 // operator = button.value;
                 // equation.textContent = x + button.value;
-                return x;
+                return;
             }
         });
     }
-    return x;
-}
-
-// makeX();
-
-function makeY() {
-    for (const button of buttons) {
-        button.addEventListener("click", () => {
-            if (
-                button.value == "1" ||
-                button.value == "2" ||
-                button.value == "3" ||
-                button.value == "4" ||
-                button.value == "5" ||
-                button.value == "6" ||
-                button.value == "7" ||
-                button.value == "8" ||
-                button.value == "9" ||
-                button.value == "0"
-            ) {
-                y += button.value;
-                // equation.textContent = x;
-                console.log(y);
-
-                // console.log(x);
-                //
-            } else if (button.value == "CA") {
-                y = "";
-                // displayResult.textContent = x;
-                // equation.textContent = x;
-            } else if (button.value == "DEL") {
-                console.log(x);
-                // turns into array
-                y = y.split("");
-                console.log(x);
-                // removes last item in array
-                y.pop();
-                console.log(y);
-                // create new variable with new array without last item
-                newY = y.join("");
-                // convert new variable to old variable as string
-                y = newY;
-                console.log(y);
-                // equation.textContent = y;
-            } else {
-                // displayResult.textContent = x;
-                // operator = button.value;
-                // equation.textContent = x + button.value;
-                return y;
-            }
-        });
-    }
-    return y;
-}
-
-function makeOperator() {
-    for (const button of buttons) {
-        button.addEventListener("click", () => {
-            if (
-                button.value == "+" ||
-                button.value == "-" ||
-                button.value == "*" ||
-                button.value == "/" ||
-                button.value == "**" ||
-                button.value == "%"
-            ) {
-                operator = button.value;
-            }
-        });
-    }
-    return operator;
-}
+}*/
